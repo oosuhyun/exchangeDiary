228fdd2fcbed7a6052679bb44ee6817baa427f48
oosuhyun <tngusimg@naver.com>
2023-08-08 17:37:54 +0900

    Initial commit

---
+++ src/test/java/com/example/exchangeDiary/ExchangeDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exchangeDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
